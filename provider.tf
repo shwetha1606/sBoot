930d714c1da4f8702961187449d728e6e6d84b38
shwetha1606 <123380957+shwetha1606@users.noreply.github.com>
2023-05-02 22:49:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,6 +4,5 @@ provider "azurerm" {
  subscription_id = "8c55c797-7123-4e4f-9857-c14ad3410eee"
  tenant_id = "bbad2ae2-20fd-46e9-bb57-97aac7b3b97f"
  client_id = "7eecf0a0-b29b-46cd-80de-61af858d8805"
- client_secrete ="c4q8Q~FR5UakFQIIJs5t5u7MKUogF22QQiok6cou"
  skip_provider_registration = true
 }
